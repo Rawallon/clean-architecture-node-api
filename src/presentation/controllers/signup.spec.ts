bc902ef3c84f8ad5c5978fde332ed00ea91c8f4c
rawallon <rawallon@gmail.com>
2021-05-25 10:57:22 -0300

    Created initial tests

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,32 @@
+import { SignUpController } from './signup'
+
+describe('Signup Controller', () => {
+  test('should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        // name: 'any-name',
+        email: 'any-email@email.com',
+        password: 'any-password',
+        confirmPassword: 'any-password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param'))
+  })
+  test('should return 400 if no emails is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any-name',
+        // email: 'any-email@email.com',
+        password: 'any-password',
+        confirmPassword: 'any-password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param'))
+  })
+})
