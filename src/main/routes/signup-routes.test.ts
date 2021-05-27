b40cc3019a7974586c5d90bbbe8ceaab33bb7c85
rawallon <rawallon@gmail.com>
2021-05-27 16:32:06 -0300

    Implemented new signup route test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,10 +1,28 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
 import app from '../config/app'
 
 describe('Signup routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('should hit signup route', async () => {
     await request(app)
       .post('/api/signup')
+      .send({
+        name: 'Wallon',
+        email: 'rawallon@gmail.com',
+        password: '123123',
+        confirmPassword: '123123'
+      })
       .expect(200)
   })
 })
